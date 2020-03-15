e0969ace3396317a042f16e52293ca5a7e3e82a9
Marzena Kaa <kacprowicz.marzena@gmail.com>
2020-03-15 19:27:12 +0100

    Create a method that consumes events from the first kafka topic, filters them and passes filtered events to the second topic.

---
+++ src/main/java/pl/maakaa/SimpleKafkaStreamsApp/SimpleKafkaStreamsAppApplication.java
@@ -0,0 +1,32 @@
+package pl.maakaa.SimpleKafkaStreamsApp;
+
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+@SpringBootApplication
+@EnableBinding(KafkaStreams.class)
+public class SimpleKafkaStreamsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleKafkaStreamsAppApplication.class, args);
+	}
+
+	/**
+	 * Method that consumes events from the first kafka topic, filters them and passes filtered events to the second topic.
+	 * @param events - a stream of events to be processed.
+	 * @return KStream - a stream of filtered events.
+	 */
+	@StreamListener(KafkaStreams.FIRST_TOPIC)
+	@SendTo(KafkaStreams.SECOND_TOPIC)
+	public KStream<?, Event> process(KStream<?, Event> events) {
+		return events
+				.filter((key, value) -> value.getType().equals(EventType.NOWY))
+				.map((key, value) -> new KeyValue<>(null, value));
+	}
+
+}
---
+++ src/test/java/pl/maakaa/SimpleKafkaStreamsApp/SimpleKafkaStreamsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maakaa.SimpleKafkaStreamsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaStreamsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
